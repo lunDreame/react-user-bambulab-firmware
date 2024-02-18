ab2c21a31b4313f8c04a5c38e54bafcb1317f069
lunDreame <87955512+lunDreame@users.noreply.github.com>
2024-02-18 12:16:16 +0900

    Add files via upload

---
+++ frontend/build/precache-manifest.c2d78b8bc2253e6254f67a285ea114cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05ef4e1a41fe75a013bd6d2dca234a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a6238d971e670af306a",
+    "url": "/static/css/main.dbbc903b.chunk.css"
+  },
+  {
+    "revision": "fe248fb50f46afb09596",
+    "url": "/static/js/2.cc7386b4.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "4a6238d971e670af306a",
+    "url": "/static/js/main.620b2835.chunk.js"
+  },
+  {
+    "revision": "8d17c9e0d9526c2c8484",
+    "url": "/static/js/runtime~main.360358fb.js"
+  }
+]);
